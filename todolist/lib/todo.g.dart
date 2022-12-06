2a322f3959766c269a30d7a5781ad9ef28d53f37
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-12-07 00:16:51 +0900

    Todo 리스트 : 완성

---
+++ todolist/lib/todo.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TodoAdapter extends TypeAdapter<Todo> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Todo read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Todo(
+      id: fields[0] as int?,
+      title: fields[1] as String,
+      dateTime: fields[2] as int,
+      isDone: fields[3] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Todo obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.dateTime)
+      ..writeByte(3)
+      ..write(obj.isDone);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TodoAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
